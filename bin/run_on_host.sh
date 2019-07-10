45323be0d4f0a3f10f64f3678042dc86dd25c751
Alexander Bersenev <bay@hackerdom.ru>
2019-07-10 16:06:10 +0500

    change the logic of parameters substitution to make it work in Ubuntu

--- bin/run_on_host.sh
+++ bin/run_on_host.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 (
-REPL_MADE=0
+INJ=0
 printf "%s" "$0 "
 for ARG; do
     NAME="$(basename -- "$ARG")"
     case $NAME in
         hydra_pmi_proxy|orted|hydra_bstrap_proxy|pmi_proxy)
-            [ $REPL_MADE == 0 ] && printf "%s" "run_in_docker.sh $IMAGE "; REPL_MADE=1;;
+            [ $INJ -eq 0 ] && printf "%s" "run_in_docker.sh $IMAGE "; INJ=1;;
     esac
-    printf "'%s' " "${ARG//\'/\'\\\'\'}"
+    printf "'"; printf "%s" "$ARG" | sed "s/'/'\\\\''/g"; printf "' "
 done
 ) >> /tmp/host_cmd
 
